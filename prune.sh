6e402af67a4346d35f60f1beaaa7611c2fa8b514
Tamás Ficsor <ficstamas@gmail.com>
2023-11-08 14:02:48 +0100

    run script

--- prune.sh
+++ prune.sh
@@ -5,37 +5,44 @@ source env/bin/activate
 #
 # Prune greedy
 #
-for task in "rte" "mrpc" "stsb" "cola" "sst2" "qnli" "qqp" "mnli"; do
-for model in "bert-base-uncased" "roberta-base"; do
+for task in "rte" "mrpc" "cola"; do
+for model in "bert-base-uncased" "roberta-base" "distilbert-base-uncased"; do
+for seed in `seq 0 1 4`; do
 python3 prune.py --model_name_or_path=$model \
     --task_name=$task \
-    --seed=41 \
-    --max_seq_length=128 \
-    --per_device_train_batch_size=32 \
+    --dataset_name="glue" \
+    --task_type="sequence" \
+    --seed=$seed \
+    --max_seq_length=256 \
+    --per_device_train_batch_size=16 \
     --learning_rate=2e-5 \
     --output_dir=experiments/tmp/ \
     --logging_dir=experiments/tmp/ \
     --prune_n_layers=6 \
     --prune_method="greedy" \
-    --overwrite_output_dir
+    --overwrite_output_dir \
+    --prune_all_but_one
+done
 done
 done
 
-
-#
-# Prune optimal
-#
-for task in "sst2" "cola" "mnli" "mrpc" "qnli" "qqp" "rte" "stsb"; do
-for model in "bert-base-uncased" "roberta-base"; do
-python3 analyze.py --model_name_or_path=$model \
+for task in "ner" "pos"; do
+for model in "bert-base-uncased" "roberta-base" "distilbert-base-uncased"; do
+for seed in `seq 0 1 4`; do
+python3 prune.py --model_name_or_path=$model \
     --task_name=$task \
-    --seed=41 \
-    --max_seq_length=128 \
-    --per_device_train_batch_size=32 \
+    --dataset_name="conll2003" \
+    --task_type="token" \
+    --seed=$seed \
+    --max_seq_length=256 \
+    --per_device_train_batch_size=16 \
     --learning_rate=2e-5 \
     --output_dir=experiments/tmp/ \
     --logging_dir=experiments/tmp/ \
-    --prune_n_layers=2 \
-    --prune_method="optimal" \
-    --overwrite_output_dir
+    --prune_n_layers=6 \
+    --prune_method="greedy" \
+    --overwrite_output_dir \
+    --prune_all_but_one
+done
+done
 done
